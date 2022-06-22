33df4cd8f8fc6a78cd60d67376152059b5351510
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2022-06-22 23:20:42 +0900

    react-hook-form (1)

---
+++ pages/forms.tsx
@@ -0,0 +1,43 @@
+import { useForm } from "react-hook-form";
+
+// Less code (c)
+// Better validation
+// Better Errors (set, clear, display)
+// Have control over inputs
+// Don`t deal with Events (c)
+// Easier inputs (c)
+
+export default function Forms() {
+  const { register, handleSubmit } = useForm();
+
+  const onValid = () => {
+    console.log("im valid bby");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onValid)}>
+      <input
+        {...register("username", {
+          required: true,
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", {
+          required: true,
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", {
+          required: true,
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
